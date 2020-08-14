14444fb8015c382b72c12cd33a1ce3ae501e0403
ocenandor <69635204+ocenandor@users.noreply.github.com>
2020-08-14 20:37:22 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,73 +1,95 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Total from './components/Total/Total';
+import History from './components/History/History';
+import Operation from './components/Operation/Operation';
 
-function App() {
-  return (
-    <React.Fragment>
+class App extends Component {
 
-      <header>
-        <h1>Кошелёк</h1>
-        <h2>Калькулятор расходов</h2>
-      </header>
 
-      <main>
-        <div className="container">
 
-          <section className="total">
-            <header className="total__header">
-              <h3>Баланс</h3>
-              <p className="total__balance">0 ₽</p>
-            </header>
+  state = {
+    transactions: [],
+    description: '',
+    amount: '',
+    consumption: 0,
+    income: 0,
+    balance: 0,
+  }
 
-            <div className="total__main">
-              <div className="total__main-item total__income">
-                <h4>Доходы</h4>
-                <p className="total__money total__money-income">
-                  +0 ₽
-                </p>
-              </div>
-              <div className="total__main-item total__expenses">
-                <h4>Расходы</h4>
-                <p className="total__money total__money-expenses">
-                  -0 ₽
-                </p>
-              </div>
-            </div>
-          </section>
+  addTransaction = add => {
+    const transactions = [...this.state.transactions];
+    const transaction = {
+      id: `cmr${(+new Date).toString(16)}`,
+      description: this.state.description,
+      amount: this.state.amount,
+      add
+    }    
+    
+    this.ChangeBalance(add, transaction.amount);
+    transactions.push(transaction);
+    
+    this.setState({
+      transactions,
+      description: '',
+      amount: '',
+    });
+  }
 
-          <section className="history">
-            <h3>История расходов</h3>
-            <ul className="history__list">
-              <li className="history__item history__item-plus">Получил зарплату
-                        <span className="history__money">+30000 ₽</span>
-                <button className="history__delete">x</button>
-              </li>
-              <li className="history__item  history__item-minus">Отдал долг
-                        <span className="history__money">-10000 ₽</span>
-                <button className="history__delete">x</button>
-              </li>
-            </ul>
-          </section>
 
-          <section className="operation">
-            <h3>Новая операция</h3>
-            <form id="form">
-              <label>
-                <input type="text" className="operation__fields operation__name" placeholder="Наименование операции"></input>
-              </label>
-              <label>
-                <input type="number" className="operation__fields operation__amount" placeholder="Введите сумму"></input>
-              </label>
-                  
-              <div className="operation__btns">
-                <button type="submit" className="operation__btn operation__btn-subtract">РАСХОД</button>
-                <button type="submit" className="operation__btn operation__btn-add">ДОХОД</button>
-              </div>
-            </form>
-          </section>
-        </div>
-      </main>
-    </React.Fragment>
-  );
+  addAmount = e => {
+
+    this.setState({ amount: e.target.value });
+  }
+  addDescription = e => {
+
+    this.setState({ description: e.target.value });
+  }
+
+  ChangeBalance = (add, amount) => {
+    var bal = this.state.balance;
+    var cons = this.state.consumption;
+    var inc = this.state.income;
+
+    if (add) {
+      bal += Number(amount);
+      inc += Number(amount);
+    }
+    else {
+      bal -= Number(amount);
+      cons += Number(amount);
+    }
+    this.setState({ balance: bal, income: inc, consumption: cons });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <header>
+          <h1>Кошелёк</h1>
+          <h2>Калькулятор расходов</h2>
+        </header>
+
+        <main>
+          <div className="container">
+            <Total
+              balance={this.state.balance} 
+              income={this.state.income} 
+              consumption={this.state.consumption} />
+            <History transactions={this.state.transactions} />
+            <Operation
+              addTransaction={this.addTransaction}
+              addAmount={this.addAmount}
+              addDescription={this.addDescription}
+              description={this.state.description}
+              amount={this.state.amount}
+              changeBalance={this.changeBalance}
+            />
+          </div>
+        </main>
+      </React.Fragment>
+
+    );
+  }
 }
 
 export default App;
